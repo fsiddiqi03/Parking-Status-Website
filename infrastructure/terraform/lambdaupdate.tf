84937bac930c1e4a3f2be4ccd50ec304aa6abd87
Faris Siddiqi <farisiddiqi@gmail.com>
2024-03-18 20:44:49 -0500

    updated REST  api to HTTP, created update status Lambda Function

--- infrastructure/terraform/lambdaupdate.tf
+++ infrastructure/terraform/lambdaupdate.tf
@@ -1,13 +1,53 @@
 resource "aws_lambda_function" "update_status" {
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
-  filename      = "updateParkingStatus"
-  function_name = "lambda_function_name"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = lambda_function.lambda_handler
+  function_name = "updateParkingStatus"
+  handler       = "lambda_function.lambda_handler"
+  role          = aws_iam_role.lambda_exec.arn
+  runtime       = "python3.8"
+  filename      = "lambda_function.zip"
 
-  source_code_hash = data.archive_file.lambda.output_base64sha256
+  source_code_hash = filebase64sha256("lambda_function.zip")
+}
 
-  runtime = "python3.8"
+/*
+Update Iam role, needs to be a @data not @resource 
 
+
+*/
+
+resource "aws_iam_role" "lambda_exec" {
+
+  name = "lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_dynamodb_access" {
+  name = "lambda_dynamodb_access"
+  role = aws_iam_role.lambda_exec.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:dynamodb:*:*:table/ParkingStatus"
+      },
+    ]
+  })
 }
--- infrastructure/terraform/restapi.tf
+++ infrastructure/terraform/restapi.tf
@@ -1,53 +1,60 @@
-resource "aws_api_gateway_rest_api" "this" {
-  name        = "ParkingStatusAPI"
-  description = "API for updating parking status from the garage system"
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-  body = jsonencode({
-    openapi = "3.0.1"
-    info = {
-      title   = "ParkingStatusAPI"
-      version = "1.0"
-    }
-    paths = {
-      "/updateStatus" = {
-        post = {
-          summary     = "Updates parking status"
-          operationId = "updateParkingStatus"
-          x-amazon-apigateway-integration = {
-            uri                  = aws_lambda_function.update_status.invoke_arn
-            httpMethod           = "POST"
-            type                 = "AWS_PROXY"
-            payloadFormatVersion = "1.0"
-          }
-          responses = {
-            "200" = {
-              description = "Status updated successfully"
-            }
-          }
-        }
+resource "aws_apigatewayv2_api" "lambda" {
+  name          = "serverless_lambda_gw"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  name        = "serverless_lambda_stage"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
       }
-    }
-  })
-}
-
-resource "aws_api_gateway_deployment" "this" {
-  rest_api_id = aws_api_gateway_rest_api.this.id
-  description = "Initial deployment"
-
-  triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.this.body))
-  }
-
-  lifecycle {
-    create_before_destroy = true
+    )
   }
 }
 
-resource "aws_api_gateway_stage" "this" {
-  deployment_id = aws_api_gateway_deployment.this.id
-  rest_api_id   = aws_api_gateway_rest_api.this.id
-  stage_name    = "prod"
+resource "aws_apigatewayv2_integration" "update" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  integration_uri    = aws_lambda_function.update_status.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "update" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  route_key = "POST /update"
+  target    = "integrations/${aws_apigatewayv2_integration.update.id}"
+}
+
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.update_status.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
 }
 
