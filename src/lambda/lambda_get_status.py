1d3ea0597285dabe4bdbccd4ce2b0db057e4c491
Faris Siddiqi <farisiddiqi@gmail.com>
2024-03-21 21:12:55 -0500

    added error handling to lambda script

--- src/lambda/lambda_get_status.py
+++ src/lambda/lambda_get_status.py
@@ -1,19 +1,30 @@
 import boto3
+import json
 
 # Initialize Boto3 DynamoDB client
 dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
 
-def main():
+def lambda_status_handler(event, context):
     table_name = 'GarageStatus'
     table = dynamodb.Table(table_name)
 
+    body = json.loads(event['body'])
+
+    GarageID = body.get('GarageID')
+    GarageName = body.get('Name')
+
+    if not GarageID or not GarageName:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'GarageID and Name are required fields'})
+        }
 
     try:
         # Retrieve item from DynamoDB table using partition key and sort key 
         response = table.get_item(
             Key={
-                'GarageID': '01',
-                'GarageName': 'Main'  
+                'GarageID': GarageID,
+                'GarageName': GarageName  
             }
         )
 
@@ -21,12 +32,21 @@ def main():
         status = response.get('Item', {}).get('Status')
 
         if status:
-            print("Current status:", status)
+            return {
+                'statusCode': 200,
+                'body': json.dumps(status)
+            }
         else:
-            print("Status not found.")
+            return {
+                'statusCode': 404,
+                'body': json.dumps({'error': 'Status not found'})
+            }
 
     except Exception as e:
-        print("Error:", e)
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
 
-main()
+
 
