aa456738338b9e78972422eceae6fbd2e4a5682e
fsiddiqi03 <farissiddiqi@gmail.com>
2024-03-22 22:05:05 -0500

    added card for garage status display

--- src/components/ParkingStatus.js
+++ src/components/ParkingStatus.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
+import heroCardCompleteImage from '../images/hero-card-complete.jpeg';
+
 
 const ParkingStatus = () => {
   return (
-    <div>ParkingStatus</div>
-  )
+    <Card className="py-4" style={{ width: '300px' }}>
+    <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+      <p className="text-tiny uppercase font-bold">Garage</p>
+      <small className="text-default-500">Main</small>
+      <h4 className="font-bold text-large">Status</h4>
+    </CardHeader>
+    <CardBody className="overflow-visible py-2">
+      <Image
+        alt="Card background"
+        className="object-cover rounded-xl"
+        src={heroCardCompleteImage}
+        width={270}
+      />
+    </CardBody>
+  </Card>
+);
 }
 
 export default ParkingStatus
