a8b3cce956016d0c5a999312ecacc4e7eeb54693
fsiddiqi03 <farissiddiqi@gmail.com>
2024-03-22 22:06:03 -0500

    adjusted index.csss and index.js for NextUI

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import ParkingStatus from './components/ParkingStatus';
+import {NextUIProvider} from "@nextui-org/react";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ParkingStatus/>
-  </React.StrictMode>
+  <NextUIProvider>
+    <React.StrictMode>
+      <ParkingStatus/>
+    </React.StrictMode>
+  </NextUIProvider>
+  
 );
+
 
 
