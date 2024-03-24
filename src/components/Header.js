91059cdfb62fbe6a496452a7e299470b72bde751
fsiddiqi03 <farissiddiqi@gmail.com>
2024-03-24 15:29:22 -0500

    created header jsx file and csss file

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header className="luc-parking-header">
+      LUC PARKING STATUS
+    </header>
+  );
+};
+
+export default Header
