ba2a7a063099cbde05583c6acfeb0643d29ce68a
fsiddiqi03 <farissiddiqi@gmail.com>
2024-03-24 15:29:50 -0500

    created footer js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer style={{ padding: '20px', backgroundColor: '#f5f5f5' }}>
+
+    </footer>
+  )
+}
+
+export default Footer
