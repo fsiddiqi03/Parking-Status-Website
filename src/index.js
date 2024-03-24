590e7fff0fe8b39685de5ed17bd1c0404acebee6
fsiddiqi03 <farissiddiqi@gmail.com>
2024-03-24 15:30:38 -0500

    updated background color, and added footer and header components

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import ParkingStatus from './components/ParkingStatus';
 import {NextUIProvider} from "@nextui-org/react";
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <NextUIProvider>
     <React.StrictMode>
+      <Header/> 
       <ParkingStatus/>
+      <Footer/>
     </React.StrictMode>
   </NextUIProvider>
   
