642d95133af753caec77aa0c59bee83273401ea3
fsiddiqi03 <farissiddiqi@gmail.com>
2024-04-11 20:11:44 -0500

    created comments for adjusting header to NavBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import './Header.css'
 
+/*
+
+ - turn into nav bar through material ui or nextui 
+
+*/
+
+
 const Header = () => {
   return (
     <header className="luc-parking-header">
